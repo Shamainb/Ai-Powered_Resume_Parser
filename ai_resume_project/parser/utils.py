6837c1e55434e0b8fcc0d0efeb20274be4842191
Shamainb <shamainbhengesa60@gmail.com>
2025-03-06 11:58:39 +0200

    Initial commit

--- ai_resume_project/parser/utils.py
+++ ai_resume_project/parser/utils.py
@@ -1,11 +1,61 @@
+from django.conf import settings
 from django.core.files.uploadedfile import UploadedFile
+from django.core.exceptions import ValidationError
 import pymupdf
+import boto3
+import json
 
 def read_uploaded_file(file_object: UploadedFile, file_extension: str)-> str:
-    with pymupdf.open(streamfile_object.read(), filetype= file_extension) as document:
-        text = **
+    with pymupdf.open(stream=file_object.read(), filetype=file_extension) as document:
+        text = ""
         for page_num in range(len(document)):
-            page = document_load_page(page_num)
+            page = document.load_page(page_num)
             text += page.get_text()
             
     return text
+
+ai_credentials_options = {
+    "region_name": settings.GEN_AI_REGION,
+    "aws_access_key_id": settings.GEN_AI_ACCESS_KEY,
+    "aws_secret_access_key": settings.GEN_AI_SECRET_KEY
+}
+
+prompt_data = """
+You are an bot designed to act as a professional for parsing resumes. You are given a resume and your job is extract the following information from the resume:
+
+1. applicant_name: ""
+2. highest_level_of_education: ""
+3. area_of_study: ""
+4. institution: ""
+5. introduction: ""
+6. skills: string {}
+7. english_proficiency_level: ""
+8. experiences: [{"employer_name":"", role:"", duration: ""}]
+
+Give the extracted info in JSON format only
+Note: if the is not present, leave the field blank
+Give 
+""" 
+def extract_resume_info(text: str):
+    try:
+        bedrock = boto3.client("bedrock-runtime", **ai_credentials_options)
+        payload = {
+            "prompt": "[INST]" + prompt_data + "Resume Content::" + text + "[INST]",
+            "max_gen_len": 1024,
+            "temperature": 0.5,
+            "top_p": 0.9
+        }
+        body = json.dumps(payload)
+        model_id = "meta.llama2-70b-chat-v1"
+        
+        response = bedrock.invoke_model(
+            body=body,
+            modelId=model_id,
+            accept="application/json",
+            contentType="application/json",
+        )
+        response_body = json.loads(response.get("body").read())
+        response_text: str = response_body["generation"]
+        print("response", response_text)
+    except Exception as e:
+        raise ValidationError(f"Something went wrong: {e}, 500")
--- ai_resume_project/parser/views.py
+++ ai_resume_project/parser/views.py
@@ -3,7 +3,8 @@ from django.http import HttpRequest, JsonResponse
 from .validators import validate_file_size, validate_file_extension
 from django.core.files.uploadedfile import UploadedFile
 from django.core.exceptions import ValidationError
-from .utils import read_uploaded_file
+from .utils import read_uploaded_file, extract_resume_info
+
 # Create your views here.
 def home(request):
     return render (request, "index.html")
@@ -15,7 +16,7 @@ def upload_resume(request: HttpRequest):
             ext: str = validate_file_extension(file)
             validate_file_size(file)
             file_content = read_uploaded_file(file, ext)
-            print("file content>>", file_content)
+            data = extract_resume_info(file_content)
         except ValidationError as err:
             return JsonResponse({"error": err.message})
         return JsonResponse({"error": "Invalid Request"}, status=400)
